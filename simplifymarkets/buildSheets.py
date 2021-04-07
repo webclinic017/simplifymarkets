5169d1a2d74dee42bdc84126ddd1ecbddb8a0ea0
himanshueip <naithani.himanshu53@gmail.com>
2021-04-08 01:38:26 +0530

    Add indexes and symbols parser
    selector in main is variable used to select different indexes from array list index

--- simplifymarkets/buildSheets.py
+++ simplifymarkets/buildSheets.py
@@ -7,6 +7,11 @@ from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
 from googleapiclient.errors import HttpError
 from apiclient import errors
+import openpyxl
+from pathlib import Path
+import time
+import json
+import re
 
 # LIST all the FILES
 def retrieve_all_files(service):
@@ -56,8 +61,8 @@ def createSheet(service):
         }
     }
     spreadSheet = service.spreadsheets().create(body=sheet, fields='spreadsheetId').execute()
-    spreadSheetId = str(sheet.get('spreadsheetId'))
-    print('Spreadsheet ID: {0}'.format(spreadSheet.get('spreadsheetId')))
+    spreadSheetId = str(spreadSheet.get('spreadsheetId'))
+    print('Spreadsheet ID: {0}'.format(spreadSheetId))
     return spreadSheetId
     
 # WRITE 
@@ -72,6 +77,49 @@ def writeSheet(service, spreadSheetId, symbol = "ITC"):
     result = service.spreadsheets().values().update(spreadsheetId=spreadSheetId, range=range_name, valueInputOption='USER_ENTERED', body=body).execute()
     #print('{0} cells updated.'.format(result.get('updatedCells')))
 
+def stockSymbols(selector):
+    SYMBOLS_PATH_excel = "symbols"
+    xlsx_file = Path(SYMBOLS_PATH_excel, selector)
+    print(xlsx_file)
+    wb_obj = openpyxl.load_workbook(xlsx_file)
+    # Read the active sheet:
+    sheet = wb_obj.active
+    symbols = sheet.max_row
+    print(symbols)
+    stock_symbols = []
+    for i in range(2, symbols + 1):
+        raw_symbol = "A" + str(i)
+        raw_data = sheet[raw_symbol].value
+        raw_data = raw_data.split(", ")[1]
+        raw_data = raw_data.split('"')[1]
+        raw_data = raw_data.split('_')[1]
+        raw_data = raw_data.split('-')[0]
+        stock_symbols.insert(i, raw_data)
+    return stock_symbols
+
+def json_writer(thisdict, index):
+    filename = index + ".json"
+    a_file = open(filename, "w")
+    json.dump(thisdict, a_file)
+    a_file.close()
+
+    a_file = open(filename, "r")
+    output = a_file.read()
+    print(output)
+    a_file.close()
+
+def listIndex(selectIndex):
+    index = ["BSEAuto MW.xlsx","BSEFMC MW.xlsx", "BSEIT MW.xlsx", "BSEMetal MW.xlsx", "BSEOnG MW.xlsx", "BSEPower MW.xlsx","BSERealty MW.xlsx", "Nifty50 MW.xlsx","NiftyAuto MW.xlsx","NiftyBank MW.xlsx","NiftyCommodities MW.xlsx", "NiftyConsumption MW.xlsx", "NiftyEnergy MW.xlsx","NiftyFMCG MW.xlsx","NiftyInfra MW.xlsx", "NiftyIT MW.xlsx","NiftyMedia MW.xlsx","NiftyMetal MW.xlsx","NiftyMidCap50 MW.xlsx","NiftyMNC MW.xlsx","NiftyNext50 MW.xlsx","NiftyPharma MW.xlsx","NiftyPSE MW.xlsx","NiftyPSUBank MW.xlsx","NiftyRealty MW.xlsx","NiftyServSector MW.xlsx"]
+    return index[selectIndex]
+
+def symbolsSheet(sheetService, stock_symbols, indexname):
+    thisdict = {}
+    for i in range(len(stock_symbols)):
+        spreadSheetId = createSheet(sheetService)
+        time.sleep(4)
+        thisdict[stock_symbols[i]] = spreadSheetId
+        json_writer(thisdict,indexname)
+    return thisdict
 
 def main():
     SCOPES = ['https://www.googleapis.com/auth/sqlservice.admin', 
@@ -88,7 +136,17 @@ def main():
     #changePermission(driveService, '1XucNbcohP3Z_e8kYcL9odGlE1Qx_vFGyCeNYQhLVDgk', )
     #downloadFile(sheetService)
     #spreadSheetId = createSheet(sheetService)
-    retrieve_all_files(driveService)
+    # dictonary or hash table key = symbol and value = sheel id
+    #link to excel to reterive symbols C:\Zerodha\Pi\LinkExcel
+
+    selector = 3
+    indexname = str(listIndex(selector))
+    print(indexname)
+    stock_symbols = stockSymbols(indexname)
+    symbolsSheet(sheetService, stock_symbols, indexname)
+
+
+    #retrieve_all_files(driveService)
     #delete everything
     # get a list stock symbols - 
     #insert_permission(service = driveService, file_id = '1HBbxqzPvGmv6ml4uxH4ZK6d3ac3n59AecJoQ7oaUZYg', permission_type = 'anyone', role = 'writer')
