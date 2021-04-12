129958dbb200412d3f64c2a379d3e6cc76fca4e5
raauhl <rahul25rocker@gmail.com>
2021-04-13 00:29:04 +0530

    restructure

--- simplifymarkets/wsgi.py
+++ simplifymarkets/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dexter project.
+WSGI config for simplifymarkets project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dexter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplifymarkets.settings')
 
 application = get_wsgi_application()
