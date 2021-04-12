effe832eed407635ed6172291f55e672df238a13
raauhl <rahul25rocker@gmail.com>
2021-04-12 23:59:29 +0530

    restructuring project

--- dexter/asgi.py
+++ dexter/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for simplifymarkets project.
+ASGI config for dexter project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplifymarkets.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dexter.settings')
 
 application = get_asgi_application()
