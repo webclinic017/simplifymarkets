a372cdb2b60f83c356b0c7b0c9e4abcb37cf5c7b
RAHUL CHAUHAN <rahul25rocker@gmail.com>
2021-04-17 13:10:16 +0530

    Create sma.py

---
+++ backend/app/app/indicators/sma.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+import backtrader as bt
+
+# Create a subclass of Strategy to define the indicators and logic
+
+class VolumeWeightedAveragePrice(bt.Strategy):
+    plotinfo = dict(subplot=False)
+
+    params = (('period', 30), )
+
+    alias = ('VWAP', 'VolumeWeightedAveragePrice',)
+    lines = ('VWAP',)
+    plotlines = dict(VWAP=dict(alpha=0.50, linestyle='-.', linewidth=2.0))
+
+    def __init__(self):
+        # Before super to ensure mixins (right-hand side in subclassing)
+        # can see the assignment operation and operate on the line
+        cumvol = bt.ind.SumN(self.data.volume, period = self.p.period)
+        typprice = ((self.data.close + self.data.high + self.data.low)/3) * self.data.volume
+        cumtypprice = bt.ind.SumN(typprice, period=self.p.period)
+        self.lines[0] = cumtypprice / cumvol
+
+        super(VolumeWeightedAveragePrice, self).__init__()
+
+    def next(self):
+        if not self.position:  # not in the market
+            if self.crossover > 0:  # if fast crosses slow to the upside
+                self.buy()  # enter long
+
+        elif self.crossover < 0:  # in the market & cross to the downside
+            self.close()  # close long position
+
+
+class SmaCross(bt.Strategy):
+    # list of parameters which are configurable for the strategy
+    params = dict(
+        pfast=10,  # period for the fast moving average
+        pslow=30   # period for the slow moving average
+    )
+
+    def __init__(self):
+        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
+        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
+        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+
+    def next(self):
+        if not self.position:  # not in the market
+            if self.crossover > 0:  # if fast crosses slow to the upside
+                self.buy()  # enter long
+
+        elif self.crossover < 0:  # in the market & cross to the downside
+            self.close()  # close long position
+
+
+cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
+
+# Create a data feed
+data = bt.feeds.YahooFinanceData(dataname='../resources/MAHABANK.NS.csv',
+                                 fromdate=datetime(2020, 1, 1),
+                                 todate=datetime(2021, 3, 21))
+
+cerebro.adddata(data)  # Add the data feed
+
+cerebro.addstrategy(SmaCross)  # Add the trading strategy
+cerebro.run()  # run it all
+cerebro.plot()  # and plot it with a single command 
