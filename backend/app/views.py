dbb4978d180689ae41b3d28b1c2b7dd9ecda596b
raauhl <rahul25rocker@gmail.com>
2021-05-08 17:18:55 +0530

    Adding binance data api

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .coinbase.Login import *
+import json
+
+COINBASE_API_KEY = 'lqWSwZ85ldfoxV4p'
+COINBASE_API_SECRET = 'msSH6rRIZJ4GpjMIaNC3Vk1I2lG00oZl'
 
 def index(request):
-    return HttpResponse("Good Job!")
+    blob = execute()
+    blob = json.dumps(blob, indent=4, sort_keys=True)
+    return HttpResponse(str(blob))
     
+
+
+def execute():
+    api_url = 'https://api.coinbase.com/v2/'
+    auth = ExchangeAuth(COINBASE_API_KEY, COINBASE_API_SECRET)
+    r = requests.get(api_url + 'user', auth=auth)
+    return r.json()
