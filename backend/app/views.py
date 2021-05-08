f3945a9e6fa6f0e5225469c5fe1228621ebf2195
raauhl <rahul25rocker@gmail.com>
2021-05-08 17:21:33 +0530

    nit

--- backend/app/views.py
+++ backend/app/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 from .coinbase.Login import *
 import json
 
-COINBASE_API_KEY = 'lqWSwZ85ldfoxV4p'
-COINBASE_API_SECRET = 'msSH6rRIZJ4GpjMIaNC3Vk1I2lG00oZl'
-
 def index(request):
     blob = execute()
     blob = json.dumps(blob, indent=4, sort_keys=True)
