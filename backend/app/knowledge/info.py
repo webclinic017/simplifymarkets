861bfd7e872e42694f131b101f625c2217c08ea0
raauhl <rahul25rocker@gmail.com>
2021-05-20 15:36:52 +0530

    database and restapi test

--- backend/app/knowledge/info.py
+++
@@ -1,5 +0,0 @@
-start_date_epoch = 0
-
-CAGR = 0.0
-
-High = 0.0
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,17 +1,28 @@
+from . serializers import employeesSerializer
+from . models import employee
 from django.http import HttpResponse
-from django.shortcuts import render
-from .coinbase.Login import *
-import json
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 def index(request):
-    blob = execute()
-    blob = json.dumps(blob, indent=4, sort_keys=True)
-    return HttpResponse(str(blob))
-    
+    return HttpResponse("Awesome!")
 
-
+"""
+from .coinbase.Login import *
 def execute():
+    # blob = json.dumps(blob, indent=4, sort_keys=True)
     api_url = 'https://api.coinbase.com/v2/'
     auth = ExchangeAuth(COINBASE_API_KEY, COINBASE_API_SECRET)
     r = requests.get(api_url + 'user', auth=auth)
     return r.json()
+"""
+
+class employeeList(APIView):
+
+    def get(self, request):
+        employees1 = employee.objects.all()
+        result = employeesSerializer(employees1, many = True)
+        return Response(result.data)
+
+    def post(self):
+        return 
