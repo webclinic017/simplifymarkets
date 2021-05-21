26ea61b04c3120c799a95d159f9d766789c6bd23
raauhl <rahul25rocker@gmail.com>
2021-05-21 17:45:31 +0530

    refactor google services

--- backend/app/dataSources/gFinance/StockDataFeed.py
+++ backend/app/dataSources/gFinance/StockDataFeed.py
@@ -1,33 +1,32 @@
-import os, json
-import googleService
+from ... googleServices.GoogleService import GoogleService
+from ... googleServices.SheetsService import SheetsService
+from ... resources import feed
 
 
-""" Class to provide stock data. """
 
 class StockDataFeed:
+    """ Class to provide stock data. """
 
     # List of the symbols trader on binance
-    symbols = None
+    symbol_table = None
 
     # GoogleService
     googleService = None
 
+    sheetsService = None
+
     """ Constructor. """
     def __init__(self):
-        #self.__set_symbols()
-        #googleService = GoogleService('','')
-        print()
-    
+        self.__set_symbols()
+        StockDataFeed.googleService = GoogleService()
+        StockDataFeed.sheetsService = SheetsService(StockDataFeed.googleService.get_sheets_service())
+        self.get_daily_stock_data('ITC')
 
     """ Fetches list of the symbols currently traded on NSE. """
     def __set_symbols(self):
-        if StockDataFeed.symbols is None:
-            StockDataFeed.symbols = self.get_stockdb_index()
+        if StockDataFeed.symbol_table is None:
+            StockDataFeed.symbol_table = feed.get_symbols_info()
 
-    
-    def get_daily_stock_data():
-        print()
-
-
-s = StockDataFeed()
-print()
+    def get_daily_stock_data(self, symbol):
+        data = StockDataFeed.sheetsService.read_sheet(StockDataFeed.symbol_table[symbol])
+        print(data)
--- backend/app/utility/File.py
+++ backend/app/utility/File.py
@@ -7,19 +7,20 @@ import json
 class File:
 
     """ Read file and return object. """
-    def read(self, file_name, extension, path = None, permission = 'r'):
+    def read(self, file_name, extension, path = None):
         assert (file_name != '')
         assert (extension != '')
-
+        if path is None:
+            path = os.getcwd()
         if path[-1] != '/':
             path += '/'
 
         absolute_file_name = path + file_name + '.' + extension
         file = None
-
+        file_data = None
+        
         try:
-            file = open(absolute_file_name, permission)
-            file_data = None
+            file = open(absolute_file_name, mode = 'r')
             if extension == 'csv':
                 reader = csv.reader(file)
                 file_data = []
@@ -41,3 +42,5 @@ class File:
         fileObj = open(path + fileName + '.' + extension, permission)
         json.dump(data, fileObj)
         fileObj.close()
+
+#d = f.read(path = 'D:\\simplifymarkets\\backend\\app\\resources', file_name='database_index', extension='json')
